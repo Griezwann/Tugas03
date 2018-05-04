fdc45bef26db69843a4c865e1da029db79b168fe
griezwann <Cuanzplakk@gmail.com>
2018-05-04 12:55:09 +0800

    Menggunakan method set

--- src/tugas03/handphoneAksi.java
+++ src/tugas03/handphoneAksi.java
@@ -2,9 +2,11 @@ package tugas03;
 public class handphoneAksi {
     public static void main(String[] args) {
         handphone h = new handphone();
-        h.merk="samsung";
-        h.warna="putih";
-        h.harga=5000000;
+        
+        h.setMerk("samsung");
+        h.setWarna("putih");
+        h.setHarga(5000000);
+        
         
         h.cetakInfo();
     
