3d11656585d18c3364a1d6abd6a64ba8a1225e1d
griezwann <Cuanzplakk@gmail.com>
2018-05-04 12:45:41 +0800

    Instance,buat atribut dan panggil method

--- src/tugas03/handphoneAksi.java
+++ src/tugas03/handphoneAksi.java
@@ -1,6 +1,12 @@
 package tugas03;
 public class handphoneAksi {
     public static void main(String[] args) {
+        handphone h = new handphone();
+        h.merk="samsung";
+        h.warna="putih";
+        h.harga=5000000;
+        
+        h.cetakInfo();
     
     }
     
